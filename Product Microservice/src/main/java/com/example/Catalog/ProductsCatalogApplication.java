f0178d4e3b730d249f63d3d0d5b7defe47378ec8
Aditya  Sunit Kanoi <adityakanoi123@gmail.com>
2024-04-19 19:47:56 +0530

    Changed Server from Tomcat to Jetty

--- Product Microservice/src/main/java/com/example/Catalog/ProductsCatalogApplication.java
+++ Product Microservice/src/main/java/com/example/Catalog/ProductsCatalogApplication.java
@@ -1,5 +1,6 @@
 package com.example.Catalog;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -10,6 +11,7 @@ public class ProductsCatalogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductsCatalogApplication.class, args);
+		System.out.println("Server Started At" + " " +"http://localhost:8100/swagger-ui/index.html#/");
 	}
 
 }
