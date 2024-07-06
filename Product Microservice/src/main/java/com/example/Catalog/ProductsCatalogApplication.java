413b24c8a467e6da2b58a34440337a7eb51b72b9
Aditya  Sunit Kanoi <adityakanoi123@gmail.com>
2024-07-06 22:31:23 +0530

    Changed POM

--- Product Microservice/src/main/java/com/example/Catalog/ProductsCatalogApplication.java
+++ Product Microservice/src/main/java/com/example/Catalog/ProductsCatalogApplication.java
@@ -1,9 +1,8 @@
 package com.example.Catalog;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
